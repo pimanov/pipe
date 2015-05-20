db833bc6941769939a65cbe91d45b09db94b85f9
Vladimir Pimanov <pimanov.vladimir@gmail.com>
2015-05-20 13:04:22 +0300

    om calculated in prt

--- prt.for
+++ prt.for
@@ -1,7 +1,7 @@
 *
       subroutine prt(t,dt,u,v,w,p,Jmax)
       implicit real*8 (a-h,o-z)
-      complex*16 u,v,w,p,d,c0,ci,ubulk
+      complex*16 u,v,w,p,d,c0,ci,ubulk,u33,om,ampl,om1,amp
       dimension
      > u(0:Jmax,0:*)
      >,v(0:Jmax,0:*)
@@ -14,6 +14,7 @@
      >/Re/Re
      >/cf/cf
      >/alpha/alpha
+     >/om/u33,tl,ampl,amp1l
 *
       p(1,1)=p(1,1)
 *
@@ -33,20 +34,28 @@
       end do
       ubulk=ubulk/Ss
 
-      amp=0.d0
+      amp=c0
       do j=1,Jm
         do k=1,Km
-          amp=amp+yt(j)*ht*yt1(j)*abs(u(j,k))
-     >           +rt(j)*ht*rt1(j)*abs(v(j,k))
-     >           +yt(j)*ht*yt1(j)*abs(w(j,k))
+          amp=amp+yt(j)*ht*yt1(j)*u(j,k)**2
+     >           +rt(j)*ht*rt1(j)*v(j,k)**2
+     >           +yt(j)*ht*yt1(j)*w(j,k)**2
         end do
       end do
-      amp=amp*nsym
+      amp=sqrt(amp*nsym)
+ 
+      om=(log(amp)-log(ampl))/(t-tl)
+      ampl=amp
 
-      write(8,120)t,dt,amp,cf,real(ubulk),aimag(ubulk),dd
-      write(*,110)t,dt,amp,cf,real(ubulk),aimag(ubulk),dd
+      om1=(log(u(Jm/2,Km/2))-log(u33))/(t-tl)
+      tl=t
+      u33=u(Jm/2,Km/2)
+
+      write(8,120)t,dt,abs(amp),cf,ubulk,dd,om,om1
+      write(*,110)t,dt,abs(amp),cf,ubulk,dd,om,om1
       return
 120   format(15e25.15)
-110   format('t=',f10.4,',  dt=',f10.4,',  amp=',e12.4,',  cf=',e12.4,
-     > ',  Re(ub)=',e12.4,',  Im(ub)=',e12.4,',  dd=',e12.4)
+110   format('t=',f10.4,',  dt=',f10.4,',  amp=',e12.4,',  cf=',f8.4,
+     > ',  ub=(',e12.4,', ',e12.4,')',',  dd=',e12.4,
+     > ', om=(',f10.4,', ',f10.4,')',', om1=(',f10.4,', ',f10.4,')')
       end
