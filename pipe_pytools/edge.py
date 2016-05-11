9b1c1abe02482a590dc4b6ea1cb90de0a205823b
Vladimir Pimanov <pimanov.vladimir@gmail.com>
2016-05-11 10:16:22 +0300

    draft of edge.py

---
+++ pipe_pytools/edge.py
@@ -0,0 +1,58 @@
+import numpy as np
+import tools
+
+    
+def chec_lam(vel, turb_vel):
+    amp0 = np.abs(turb_vel[2,1:-1,1:-1,1:-1]).max()
+    amp = np.abs(vel[2,1:-1,1:-1,1:-1]).max()
+    if amp > 0.9*amp0: return -1
+    if amp < 0.1*amp0: return 1
+    return 0
+
+
+def check_scp_lam_tend(sname, vel, al):
+    
+    al_vel = vel*al
+    time = 0.0
+    tmax = 0.0
+    tend = 0
+    while tend == 0:
+        tmax += 200
+        tools.put_scp("tmp.scp",time,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym, al_vel)
+        tools.put_car(tmax, dt, cf, "tmp.scp")
+        
+        !rm -f a0.dat
+        ! mpirun -np 4 ./pipe.out
+        
+        time,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,vel_al = tools.get_scp("tmp.scp")
+        tend = chec_lam(vel_al, vel)
+        
+    a_name = sname + "_prt/a%s.dat" % str(n).zfill(3)
+    !mv a0.dat $a_name
+    
+    return 0 if tend == -1 else 1
+    
+    
+def init(sname, n=0, al=1.0, dal=1.0, lam_tend=0):
+    f = open(sname,"a")
+    f.write("%3d %25.22f %25.22f %d\n" % (n,al,dal,lam_tend))
+    f.close()
+    
+    
+def step(sname, vel, check_lam_tend):
+        
+    n, al, dal, lam_tend = np.loadtxt(sname+".txt", usecols=range(4), unpuck=False)[-1]
+    print sname+".txt[-1]:", n, al, dal, lam_tend
+    
+    n += 1
+    dal /= 2
+    if lam_tend == 1: 
+        al += dal
+    else: 
+        al -= dal
+        
+    lam_tend = check_lam_tend(sname, vel, al)
+    
+    f = open(sname,"a")
+    f.write("%3d %25.22f %25.22f %d\n" % (n,al,dal,lam_tend))
+    f.close()
---
+++ pipe_pytools/tools.py
@@ -0,0 +1,178 @@
+import struct
+import os
+import numpy as np
+
+
+_get_i = lambda(f): struct.unpack('i', f.read(4))[0]
+_get_d = lambda(f): struct.unpack('d', f.read(8))[0]
+
+def get_line(f,line):
+    n = line.shape[0]
+    
+    n1 = _get_i(f)
+    if (n1 != n*8): print "tools.py: get_line(): warning! fortran binary file is incorrect"
+    
+    for i in range(n): 
+        line[i] = _get_d(f)
+        
+    n1 = _get_i(f)
+    if (n1 != n*8): print "tools.py: get_line(): warning! fortran binary file is incorrect"
+
+
+def get_vfield(f,Im,Jm,Km):
+    vel = np.zeros((3, Km+2, Jm+2, Im+2), order='C')
+    
+    for k in range(1,Km+1):
+        for j in range(1,Jm+1):
+            get_line(f, vel[0,k,j,1:-1])
+            get_line(f, vel[1,k,j,1:-1])
+            get_line(f, vel[2,k,j,1:-1])
+
+    return vel
+
+def get_scp(fn):
+    i = _get_i
+    d = _get_d
+
+    with open(fn, "rb") as f:
+        _,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,_ = i(f),d(f),d(f),d(f),d(f),d(f),d(f),i(f),i(f),i(f),i(f),i(f)
+        vel = get_vfield(f, 2**lx, Jm, 2**lt)
+
+    return t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,vel
+
+
+
+def get_dcp(fn):
+    i = _get_i
+    d = _get_d
+
+    with open(fn, "rb") as f:
+        _,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,_ = i(f),d(f),d(f),d(f),d(f),d(f),d(f),i(f),i(f),i(f),d(f),i(f)
+        vel = get_vfield(f, 2**lx, Jm, 2**lt)
+
+    return t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,vel
+
+
+def get_cp(fn):
+    i = _get_i
+    d = _get_d
+
+    with open(fn, "rb") as f:
+        _,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,_ = i(f),d(f),d(f),d(f),d(f),d(f),d(f),i(f),i(f),i(f),i(f)
+        vel = get_vfield(f, 2**lx, Jm, 2**lt)
+
+    return t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,vel
+
+
+def put_line(f,line):
+    n = line.shape[0]
+    f.write(struct.pack("i",n*8))
+    for l in line: 
+        f.write(struct.pack("d",l))
+    f.write(struct.pack("i",n*8))
+    return 
+
+
+def put_vfield(f,vel):
+    Km = vel.shape[1] - 2
+    Jm = vel.shape[2] - 2
+    
+    for k in range(1,Km+1):
+        for j in range(1,Jm+1):
+            put_line(f,vel[0,k,j,1:-1])
+            put_line(f,vel[1,k,j,1:-1])
+            put_line(f,vel[2,k,j,1:-1])
+            
+    return 
+
+def put_scp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym):
+    m = 4*4+6*8
+    f.write(struct.pack("i",m))
+    f.write(struct.pack("d",t))
+    f.write(struct.pack("d",dt))
+    f.write(struct.pack("d",Dp))
+    f.write(struct.pack("d",Re))
+    f.write(struct.pack("d",Xmax))
+    f.write(struct.pack("d",epsr))
+    f.write(struct.pack("i",lx))
+    f.write(struct.pack("i",Jm))
+    f.write(struct.pack("i",lt))
+    f.write(struct.pack("i",nsym))
+    f.write(struct.pack("i",m))
+    return
+
+def put_scp(fn,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,vel):
+    
+    with open(fn,"wb") as f:
+        put_scp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym)
+        put_vfield(f,vel)
+    
+    return
+
+
+
+def put_dcp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym):
+    m = 3*4+7*8
+    f.write(struct.pack("i",m))
+    f.write(struct.pack("d",t))
+    f.write(struct.pack("d",dt))
+    f.write(struct.pack("d",Dp))
+    f.write(struct.pack("d",Re))
+    f.write(struct.pack("d",Xmax))
+    f.write(struct.pack("d",epsr))
+    f.write(struct.pack("i",lx))
+    f.write(struct.pack("i",Jm))
+    f.write(struct.pack("i",lt))
+    f.write(struct.pack("d",nsym))
+    f.write(struct.pack("i",m))
+    return
+
+def put_dcp(fn,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym,vel):
+    
+    with open(fn,"wb") as f:
+        put_dcp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,nsym)
+        put_vfield(f,vel)
+    
+    return
+
+
+def put_cp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt):
+    m = 3*4+6*8
+    f.write(struct.pack("i",m))
+    f.write(struct.pack("d",t))
+    f.write(struct.pack("d",dt))
+    f.write(struct.pack("d",Dp))
+    f.write(struct.pack("d",Re))
+    f.write(struct.pack("d",Xmax))
+    f.write(struct.pack("d",epsr))
+    f.write(struct.pack("i",lx))
+    f.write(struct.pack("i",Jm))
+    f.write(struct.pack("i",lt))
+    f.write(struct.pack("i",m))
+    return
+
+def put_cp(fn,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt,vel):
+    
+    with open(fn,"wb") as f:
+        put_cp_header(f,t,dt,Dp,Re,Xmax,epsr,lx,Jm,lt)
+        put_vfield(f,vel)
+    
+    return
+
+
+
+def put_car(tmax,dt,cf,cpfn):
+    f = open("pipe.car","w")
+    f.write("1.e-2         -tol\n")
+    f.write("10         -kprt\n")
+    f.write("10000         -kwrt\n")
+    f.write("%22.15f         -tmax\n" % tmax)
+    f.write("%18.15f         -dtmax\n" % dt)
+    f.write("%18.15f         -cf\n" % cf)
+    f.write("%s\n" % cpfn)
+    f.write("a0.dat")
+    f.close()
+    return
+
+
+
