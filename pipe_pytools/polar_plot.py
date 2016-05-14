f47cd55eeab7c2b99343245c8346cfd3bb2cb098
Vladimir Pimanov <pimanov.vladimir@gmail.com>
2016-05-14 17:28:24 +0300

    new polarplot func

--- pipe_pytools/polar_plot.py
+++ pipe_pytools/polar_plot.py
@@ -6,6 +6,88 @@ import math
 import com
 
 
+def polar_contourf(r, th, u, amp, cmap_param="jet"):
+    (umin, umax) =amp
+    
+    radius = r
+    angles = np.repeat( th[ ..., np.newaxis ], r.shape[0], axis = 1 )
+
+    x = (radius * np.cos( angles )).flatten()
+    y = (radius * np.sin( angles )).flatten()
+    triang = tri.Triangulation(x, y)
+
+    z = u.flatten()
+    
+    levels = MaxNLocator(nbins=16).tick_values(umin, umax)
+    scmap = plt.get_cmap(cmap_param)
+
+    plt.gca().set_aspect('equal')
+    plt.tricontourf( triang, z, levels = levels, cmap = scmap)
+    plt.xlim(0, 1.0)
+    plt.ylim(0, 1.0)
+
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+    return
+    
+    
+def polar_contour(r, th, u, amp, ampmax=None, nlines=1, line_param='-k'):
+
+    if type(ampmax) == type(None): 
+        ampmax = amp
+    
+    radius = r
+    angles = np.repeat( th[ ..., np.newaxis ], r.shape[0], axis = 1 )
+
+    x = (radius * np.cos( angles )).flatten()
+    y = (radius * np.sin( angles )).flatten()
+    triang = tri.Triangulation(x, y)
+
+    z = u.flatten()
+    
+    level0 = MaxNLocator(nbins=nlines).tick_values(amp, ampmax)
+
+    plt.gca().set_aspect('equal')
+    plt.tricontour( triang, z, colors=line_param, levels=level0)
+    plt.xlim(0, 1.0)
+    plt.ylim(0, 1.0)
+
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+    return
+    
+
+def polar_arrow(v, w, rs, ths, alpha, color='k'):
+    
+    def x(r,th):
+        return r * math.cos(th)
+    
+    def y(r,th):
+        return r * math.sin(th)
+    
+    def ux(v,w,th):
+        return v*math.cos(th) - w*math.sin(th)
+    
+    def uy(v,w,th):
+        return v*math.sin(th) + w*math.cos(th)
+    
+    for th, v1, w1 in zip(ths, v, w):
+        for r, v2, w2, in zip(rs, v1, w1):
+            plt.arrow(x(r,th), y(r,th), alpha*ux(v2,w2,th), alpha*uy(v2,w2,th), head_width=0.01, fc=color, ec=color)
+         
+    plt.gca().set_aspect('equal')
+    plt.xlim(0, 1.0)
+    plt.ylim(0, 1.0)
+
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+    return
+
+
+
 def sym_polar_plot( grid, u, amp):
     
     (R, O) = grid
